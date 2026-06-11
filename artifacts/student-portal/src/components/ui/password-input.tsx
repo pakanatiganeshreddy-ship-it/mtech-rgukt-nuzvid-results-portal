2793b503c72c5b86a25fe162c921e881fe5a393b
pakanatiganeshreddy-ship-it <pakanatiganeshreddy@gmail.com>
2026-06-11 19:09:12 +0530

    Create password-input.tsx

---
+++ artifacts/student-portal/src/components/ui/password-input.tsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Eye, EyeOff } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+interface PasswordInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string;
+}
+
+export function PasswordInput({ className, ...props }: PasswordInputProps) {
+  const [show, setShow] = useState(false);
+  return (
+    <div className="relative">
+      <Input
+        type={show ? "text" : "password"}
+        className={cn("pr-10", className)}
+        {...props}
+      />
+      <button
+        type="button"
+        tabIndex={-1}
+        onClick={() => setShow((s) => !s)}
+        className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-400 hover:text-gray-600 focus:outline-none"
+      >
+        {show ? <EyeOff className="h-4 w-4" /> : <Eye className="h-4 w-4" />}
+      </button>
+    </div>
+  );
+}
